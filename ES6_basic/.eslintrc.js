5d0214dc148f39c4bc1f8b0a124b1ae23fcc9d3d
salalifatima <salali.fatima@gmail.com>
2025-04-08 11:56:43 +0400

    update

--- ES6_basic/.eslintrc.js
+++ ES6_basic/.eslintrc.js
@@ -1,35 +1,38 @@
 module.exports = {
     env: {
-      browser: false,
-      es6: true,
-      jest: true,
+        browser: false, // Brauzer mühiti istifadə edilmir
+        es6: true,      // ES6 xüsusiyyətləri aktiv
+        jest: true,     // Jest mühiti aktiv
+        node: true      // Node.js mühiti aktiv
     },
     extends: [
-      'airbnb-base',
-      'plugin:jest/all',
+        'airbnb-base',
+        'plugin:jest/all',
     ],
     globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+        module: 'readonly', // module dəyişənini tanıyın
     },
     parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
+        ecmaVersion: 2018,
+        sourceType: 'module',
     },
     plugins: ['jest'],
     rules: {
-      'no-console': 'off',
-      'no-shadow': 'off',
-      'no-restricted-syntax': [
-        'error',
-        'LabeledStatement',
-        'WithStatement',
-      ],
+        'no-console': 'off',
+        'no-shadow': 'off',
+        'no-restricted-syntax': [
+            'error',
+            'LabeledStatement',
+            'WithStatement',
+        ],
     },
-    overrides:[
-      {
-        files: ['*.js'],
-        excludedFiles: 'babel.config.js',
-      }
+    overrides: [
+        {
+            files: ['*.js'],
+            excludedFiles: 'babel.config.js',
+        }
     ]
-  };
+};
+
