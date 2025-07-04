1c9df19413a40ebfdf0f31a48c9467688e4f8633
Fatima <128232490+salalifatima@users.noreply.github.com>
2025-07-04 17:05:35 +0400

    Update 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -6,7 +6,5 @@ process.stdin.on('readable', () => {
   if (chunk !== null) {
     process.stdout.write(`Your name is: ${chunk}`);
     console.log('This important software is now closing');
-    process.exit();
   }
 });
-
