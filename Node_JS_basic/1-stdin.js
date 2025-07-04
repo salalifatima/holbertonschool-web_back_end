29e7fb371eb13c47f0939ac042308ba1b1a9feee
salalifatima <salali.fatima@gmail.com>
2025-07-04 17:01:59 +0400

    add 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -6,5 +6,7 @@ process.stdin.on('readable', () => {
   if (chunk !== null) {
     process.stdout.write(`Your name is: ${chunk}`);
     console.log('This important software is now closing');
+    process.exit();
   }
 });
+
